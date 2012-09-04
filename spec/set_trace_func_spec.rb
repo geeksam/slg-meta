024a9dbace709097944187c96b3ebc1da83d87c9
Sam Livingston-Gray <geeksam@gmail.com>
2012-09-03 22:19:43 -0700

    Update specs for SetTraceFunc

--- spec/set_trace_func_spec.rb
+++ spec/set_trace_func_spec.rb
@@ -2,16 +2,22 @@ require 'spec_helper'
 
 describe "using set_trace_func" do
   subject { SLG::Meta::SetTraceFunc }
-  let(:traced_method) {
-    SLG::Meta::TracedMethod.for_method('Weeble#wobble')
-  }
+  let(:traced_method) { SLG::Meta::TracedMethod.for('Weeble#wobble') }
 
   it "traces calls to a method" do
     traced_method.should_receive(:called!).exactly(10).times
     subject.trace!(traced_method)
     10.times do
-      # SpecTargets::ForSetTraceFunc.new.foo
+      Weeble.wobble
+    end
+  end
+
+  it "traces calls to a method, blithely ignoring the difference between instance and singleton methods", :integration => true do
+    subject.trace!(traced_method)
+    5.times do
+      Weeble.wobble
       Weeble.new.wobble
     end
+    expect(traced_method.call_count).to eq(10)
   end
 end
