cfe1598da322ce638bdc52f1342da7558f9c63ee
Sam Livingston-Gray <geeksam@gmail.com>
2012-09-04 23:28:14 -0700

    Make spec_helper pathnames available in specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
 require 'pathname'
-spec_path = Pathname.new(File.dirname(__FILE__))
-lib_path  = Pathname.new(File.expand_path(spec_path + '../lib'))
-$: << lib_path.to_s
+SpecPath = Pathname.new(File.dirname(__FILE__))
+LibPath  = Pathname.new(File.expand_path(SpecPath + '../lib'))
+$: << LibPath.to_s
 require 'slg-meta'
-require spec_path + 'weeble'
-require spec_path + 'shared/wobblers'
-require spec_path + 'shared/method_tracer'
+require SpecPath + 'weeble'
+require SpecPath + 'shared/wobblers'
+require SpecPath + 'shared/method_tracer'
