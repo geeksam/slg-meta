c4e75dae73dedfc154675aaebc95f069059675cd
Sam Livingston-Gray <geeksam@gmail.com>
2012-09-04 23:59:36 -0700

    Refactor TracedMethod to defer const_lookup somewhat

--- lib/slg-meta/traced_method.rb
+++ lib/slg-meta/traced_method.rb
@@ -3,24 +3,26 @@ require 'thread'
 module SLG
   module Meta
     class TracedMethod
-      attr_reader :base, :method, :type, :call_count
+      attr_reader :base_name, :method, :type, :call_count
 
       def self.for(method_id_string)
-        method_id_string =~ /(.*)([#\.])(.*)/
-        base, type, method = $1.strip, $2, $3.strip
-        base   = Kernel.const_lookup(base)
-        type   = type == '#' ? :instance : :singleton
-        method = method.to_sym
-        new(base, type, method)
+        md = /(.*)([#\.])(.*)/.match(method_id_string)
+        base_name = md.captures[0].gsub(/\s+/, '')
+        type      = md.captures[1] == '#' ? :instance : :singleton
+        method    = md.captures[2].strip.to_sym
+        new(base_name, type, method)
       end
 
       def initialize(base, type, method)
-        @base, @type, @method = base, type, method
+        @base = base unless base.kind_of?(String) # allow setting class directly (facilitates testing)
+        @base_name = base.to_s
+        @type = type
+        @method = method
         @call_count = 0
       end
 
-      def base_name
-        base.to_s
+      def base
+        @base ||= Kernel.const_lookup(base_name)
       end
 
       def called!
@@ -34,7 +36,7 @@ module SLG
 
       def to_s
         separator = type == :instance ? '#' : '.'
-        [base, separator, method].join
+        [base_name, separator, method].join
       end
 
       private
